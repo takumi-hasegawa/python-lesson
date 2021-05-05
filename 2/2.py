c4f14f41cf5bcd6c78a9d2385918029d0755d2bd
takumi-hasegawa <hase12prog@gmail.com>
2021-05-05 15:03:28 +0900

    2問目OK

--- 2/2.py
+++ 2/2.py
@@ -5,23 +5,24 @@ while True:
 	if n == 0:
 		break
 
-	max = 0
 	prices = list(map(int, input().split()))
 	prices.sort()
-	prices.reverse()
-
-	# anがmより大きければ削除
-	for i in range(len(prices)):
-		if prices[i] >= m:
-			prices[i].pop(i)
 
 	# 要素数が2個未満の時は無理
 	if len(prices) < 2:
 		print("NONE")
 		continue
-
+    
+	max = 0
 	# 要素数が2つ以上の時　前方から組み合わせる
 	for i in range(len(prices) - 1):
 		for j in range(len(prices) - i - 1):
 			com = prices[i] + prices[j + i + 1]
-			print(com)
+			if m >= com > max:
+			    max = com
+
+	if max == 0:
+		print("NONE")
+		continue
+	
+	print(max)
