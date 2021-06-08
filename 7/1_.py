61a47a850544c9480dba0b1656de3fd953464c0d
takumi-hasegawa <hase12prog@gmail.com>
2021-06-08 22:17:13 +0900

    revise

--- 7/1_.py
+++ 7/1_.py
@@ -8,11 +8,10 @@ while True:
         
         # 隣接するノードをnext_nodesに格納する
         for k, l in move:
-            if 0 <= i+k and i+k < w and 0 <= j+l and j+l < h: # out of range にならないように
-                if (i+k, j+l) not in visited_nodes and (i+k, j+l) not in next_nodes and c[i+k][j+l] == 1:
-                    next_nodes.append((i+k, j+l))
-        
-        print(visited_nodes)
+            if 0 <= i+k and i+k < h and 0 <= j+l and j+l < w: # out of range にならないように (0<=i+k<=h, 0<=j+l<=w-1)
+                if (i+k, j+l) not in visited_nodes and (i+k, j+l) not in next_nodes and c[i+k][j+l] == 1: # visited_nodesとnext_nodesは重複させないほうがいい
+                    next_nodes.append((i+k, j+l)) 
+
         # next_nodesから1つ取り出し再帰
         while len(next_nodes) > 0:
             next_node = next_nodes.pop()
